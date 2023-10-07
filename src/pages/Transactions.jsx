40b26c9919b4555b29e6836304aa1fa11c82c5bc
tobisalau <ts.tobisalau@gmail.com>
2023-10-07 14:17:23 +0100

    Changes

--- src/pages/Transactions.jsx
+++ src/pages/Transactions.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import { GridComponent, ColumnsDirective, ColumnDirective, Resize, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit, Inject } from '@syncfusion/ej2-react-grids';
 
-const Transactions = () => (
+import { transactions, contextMenuItems, ordersGrid } from '../data/dummy';
+import { Header } from '../components';
+
+const Transactions = () => {
+  const editing = { allowDeleting: true, allowEditing: true };
+  return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
-        <p>Transactions</p>
+      <Header category="Page" title="Orders" />
+      <GridComponent
+        id="gridcomp"
+        dataSource={transactions}
+        allowPaging
+        allowSorting
+        allowExcelExport
+        allowPdfExport
+        contextMenuItems={contextMenuItems}
+        editSettings={editing}
+      >
+        <ColumnsDirective>
+          {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+          {ordersGrid.map((item, index) => <ColumnDirective key={index} {...item} />)}
+        </ColumnsDirective>
+        <Inject services={[Resize, Sort, ContextMenu, Filter, Page, ExcelExport, Edit, PdfExport]} />
+      </GridComponent>
     </div>
-);
-
+  );
+};
 export default Transactions;
