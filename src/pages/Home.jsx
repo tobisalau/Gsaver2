ef465df09f68f95e57fbb4753938c5faa67b0b48
maxwelltyreece <107719479+maxwelltyreece@users.noreply.github.com>
2023-10-07 13:37:50 +0100

    Maxwell 1.0
    Edited home page
    Edited sidebar
    Installed MUI

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,8 @@ import { GoPrimitiveDot } from 'react-icons/go';
 import { IoIosMore } from 'react-icons/io';
 import { DropDownListComponent } from '@syncfusion/ej2-react-dropdowns';
 
+import { Button } from '@mui/material';
+
 import { Stacked, Pie, Button, LineChart, SparkLine } from '../components';
 import { earningData, medicalproBranding, recentTransactions, weeklyStats, dropdownData, SparklineAreaData, ecomPieChartData } from '../data/dummy';
 import { useStateContext } from '../contexts/ContextProvider';
@@ -33,6 +35,7 @@ const Home = () => {
             >
               <BsCurrencyDollar />
             </button>
+            <Button/>
           </div>
           <div className="mt-6">
             <Button
@@ -65,89 +68,7 @@ const Home = () => {
         </div>
       </div>
 
-      <div className="flex gap-10 flex-wrap justify-center">
-        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-840  ">
-          <div className="flex justify-between">
-            <p className="font-semibold text-xl">System Temperatures</p>
-            <div className="flex items-center gap-4">
-              <p className="flex items-center gap-2 text-gray-600 hover:drop-shadow-xl">
-                <span>
-                  <GoPrimitiveDot />
-                </span>
-                <span>CPU</span>
-              </p>
-              <p className="flex items-center gap-2 text-green-400 hover:drop-shadow-xl">
-                <span>
-                  <GoPrimitiveDot />
-                </span>
-                <span>Fan Speeds</span>
-              </p>
-            </div>
-          </div>
-          <div className="mt-10 flex gap-10 flex-wrap justify-center">
-            <div className=" border-r-1 border-color m-4 pr-10">
-              <div>
-                <p>
-                  <span className="text-3xl font-semibold">60 celcius</span>
-                  <span className="p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-white bg-green-400 ml-3 text-xs">
-                    Down 30%
-                  </span>
-                </p>
-                <p className="text-gray-500 mt-1">CPU Temperature</p>
-              </div>
-              <div className="mt-8">
-                <p className="text-3xl font-semibold">3000 rpm</p>
-
-                <p className="text-gray-500 mt-1">Fan Speed</p>
-              </div>
-
-              <div className="mt-5">
-                {/* <SparkLine currentColor={currentColor} id="line-sparkLine" type="Line" height="80px" width="250px" data={SparklineAreaData} color={currentColor} /> */}
-              </div>
-              <div className="mt-10">
-                <Button
-                  color="white"
-                  bgColor={currentColor}
-                  text="Go to temps"
-                  borderRadius="10px"
-                />
-              </div>
-            </div>
-            <div>
-              <LineChart currentMode={currentMode} width="320px" height="360px" />
-            </div>
-          </div>
-        </div>
-        <div>
-          <div
-            className=" rounded-2xl md:w-400 p-4 m-3"
-            style={{ backgroundColor: currentColor }}
-          >
-            <div className="flex justify-between items-center ">
-              <p className="font-semibold text-white text-2xl">System Storage</p>
-
-              <div>
-                <p className="text-gray-200">67% full</p>
-              </div>
-            </div>
-
-            <div className="mt-4">
-              <SparkLine currentColor={currentColor} id="column-sparkLine" height="100px" type="Column" data={SparklineAreaData} width="320" color="rgb(242, 252, 253)" />
-            </div>
-          </div>
-
-          <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg rounded-2xl md:w-400 p-8 m-3 flex justify-center items-center gap-10">
-            <div>
-              <p className="text-2xl font-semibold ">Storage View</p>
-              <p className="text-gray-400">33% free</p>
-            </div>
-
-            <div className="w-40">
-              <Pie id="pie-chart" data={ecomPieChartData} legendVisiblity={false} height="160px" />
-            </div>
-          </div>
-        </div>
-      </div>
+      
 
       <div className="flex gap-10 m-4 flex-wrap justify-center">
         <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg p-6 rounded-2xl">
